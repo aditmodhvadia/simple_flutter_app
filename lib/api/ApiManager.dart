1452723cc40def6d0c02d0391e800e8c0e0dbc9f
Adit Modhvadia <dev.aditmodhvadia@gmail.com>
2019-05-11 11:51:22 +0530

    - change name for BASE_URL

--- lib/api/ApiManager.dart
+++ lib/api/ApiManager.dart
@@ -10,7 +10,7 @@ import 'package:simple_flutter_app/models/response/RegistrationResponse.dart';
 
 class ApiManager {
   static Future<LoginResponse> loginUser(LoginRequest request) async {
-    String url = '${ApiURL.BASE_LOCAL}${ApiURL.AUTHENTICATE}';
+    String url = '${ApiURL.BASE_URL}${ApiURL.AUTHENTICATE}';
     http.Response loginResponse = await NetworkManager.postWithHeader(
       header: {"Authorization": getAuthHeader(request.email, request.password)},
       url: url,
@@ -23,7 +23,7 @@ class ApiManager {
 
   static Future<RegistrationResponse> registerUser(
       RegistrationRequest request) async {
-    String url = '${ApiURL.BASE_LOCAL}${ApiURL.REGISTER}';
+    String url = '${ApiURL.BASE_URL}${ApiURL.REGISTER}';
     http.Response registerResponse = await NetworkManager.postWithHeader(
       header: {'Content-Type': 'application/json'},
       body: jsonEncode(request.toJson()),
@@ -41,7 +41,7 @@ class ApiManager {
 }
 
 class ApiURL {
-  static const BASE_LOCAL = 'http://192.168.1.4:8080/api/v1';
+  static const BASE_URL = 'http://192.168.1.4:8080/api/v1'; // Local Machine
 
   static const AUTHENTICATE = '/authenticate';
   static const REGISTER = '/users';
